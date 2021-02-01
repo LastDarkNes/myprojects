fb4bf29c8d4db66380a226b022aea88fb7f4e9db
LastDarkNeS <shark6399@gmail.com>
2021-02-01 16:16:56 +0300

    -a

--- Domik/Domik/settings.py
+++ Domik/Domik/settings.py
@@ -13,10 +13,15 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
--- Domik/newsboard/urls.py
+++ Domik/newsboard/urls.py
@@ -1,8 +1,10 @@
 
 from django.urls import path
-from django.conf.urls import include
+
 from newsboard import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.newsRender),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
