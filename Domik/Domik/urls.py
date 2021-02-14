0caa6f539cbd8a6c63d362eed17546443bd4272b
LastDarkNeS <shark6399@gmail.com>
2021-02-14 19:06:23 +0300

    Autorization and registration start

--- Domik/Domik/urls.py
+++ Domik/Domik/urls.py
@@ -22,6 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('newsboard.urls')),
+    path('registration', include('registration.urls')),
+    path('autorization', include('autorization.urls')),
 ]
 
 if settings.DEBUG:
--- Domik/newsboard/models.py
+++ Domik/newsboard/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class New(models.Model):
     title = models.CharField(max_length=30)
     description = models.TextField(null = True)
-    image = models.ImageField(upload_to='', null=True)
+    image = models.ImageField(upload_to='', null=True, blank=True)
