6665b1e625c79db2c98d69e6cd6d3ab22e97f430
LastDarkNeS <shark6399@gmail.com>
2021-02-16 21:10:54 +0300

    DateTime

--- Domik/newsboard/models.py
+++ Domik/newsboard/models.py
@@ -4,3 +4,4 @@ class New(models.Model):
     title = models.CharField(max_length=30)
     description = models.TextField(null = True)
     image = models.ImageField(upload_to='', null=True, blank=True)
+    date = models.DateTimeField(auto_now=True)
