328b6c2a09e8b94aa34928545d9f92708eaa21da
LastDarkNes <shark6399@gmail.com>
2021-05-19 00:27:16 +0300

    Working version

--- Messenger/onlineChat/views.py
+++ Messenger/onlineChat/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from .models import Messedge
-# Create your views here.
+
 def onlineChat(request):
     Messedges = Messedge.objects.all()
-    if 'q' in request.POST:
-            q = request.POST['q']
-            return render_to_response('index.html', {'q': q} )
-    else:
 
-        return render(request, "onlineChat/Chat.html", {"Messedges" : Messedges})
+
+    if request.method == 'POST':
+
+        name = request.POST['name']
+        msgtext = request.POST['msgtext']
+        new = Messedge(author = name, text = msgtext)
+        new.save()
+
+        return HttpResponseRedirect('/')
+
+    return render(request, "onlineChat/Chat.html", {"Messedges" : Messedges})
